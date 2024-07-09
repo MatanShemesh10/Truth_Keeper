9c229e6a5c302aef1257cc6c3886afb6f391bee2
Matan Shemesh <MZ6L4W@NAM.CORP.GM.COM>
2024-07-09 20:31:42 +0300

    up

--- react_website/src/app/components/ui/featuredSection/FeaturedSection.tsx
+++ react_website/src/app/components/ui/featuredSection/FeaturedSection.tsx
@@ -16,7 +16,6 @@ export const FeaturedSection: React.FC<FeaturedSectionComponentProps> = () => {
 
             <div className="col-lg-4 col-12 mb-4 mb-lg-0">
               <div className="custom-block bg-white shadow-lg">
-                <a href="topics-detail.html">
                   <div className="d-flex">
                     <div>
                       <h5 className="mb-2">Our Services</h5>
@@ -33,7 +32,6 @@ export const FeaturedSection: React.FC<FeaturedSectionComponentProps> = () => {
                     display: 'block',
                     margin: '20px auto 0',
                   }} />
-                </a>
               </div>
             </div>
 
